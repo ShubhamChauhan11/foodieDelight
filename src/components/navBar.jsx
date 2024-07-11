099e77edd2231d6b2ce42f64c2d8cb3beb11c1b4
SHUBHAM CHAUHAN <96437297+ShubhamChauhan11@users.noreply.github.com>
2024-07-11 15:02:52 +0530

    Update navBar.jsx

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,18 +2,18 @@
 
 function NavBar() {
   return (
-    <div className="w-[15%] h-[100%] border-r-2 border-gray-300 flex flex-col gap-10 py-6 items-center px-4">
-    <div className="text-orange-500 font-serif font-bold text-[20px]">
+    <div className="w-full md:w-[15%] h-auto md:h-[100%] border-b-2 md:border-b-0 md:border-r-2 border-gray-300 flex flex-col md:gap-10 gap-6 py-6 items-center px-4">
+      <div className="text-orange-500 font-serif font-bold text-[20px]">
         FOODIE DELIGHT
-    </div>
-    
-    <ul className="text-black font-bold w-full">
-        <li  className="py-3 px-4 bg-orange-300 text-orange-600 rounded-md">
-            Restaurants
+      </div>
+
+      <ul className="text-black font-bold w-full">
+        <li className="py-3 px-4 bg-orange-300 text-orange-600 rounded-md">
+          Restaurants
         </li>
-    </ul>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
